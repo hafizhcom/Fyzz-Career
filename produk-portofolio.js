39edf426b7d5a8013e8bcd1933c9af80fd7384c9
Fyzz20 <nurhayatiti1122@gmail.com>
2026-05-22 20:16:44 +0700

    Refactor popover and confirmation button logic

--- produk-portofolio.js
+++ produk-portofolio.js
@@ -1,94 +1,108 @@
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]',
+);
+const popoverList = [...popoverTriggerList].map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl),
+);
 
-    const button = document.getElementById('button');
-    const text = document.getElementById('text');
-    const tombol = document.getElementById('tombol');
-    const teks = document.getElementById('teks');
-    const header = document.getElementById('header');
-    const toggleBtn = document.getElementById('toggleBtn');
-    const footer = document.getElementById('footer');
-    const body = document.body;
+const button = document.getElementById("button");
+const text = document.getElementById("text");
+const tombol = document.getElementById("tombol");
+const teks = document.getElementById("teks");
+const header = document.getElementById("header");
+const toggleBtn = document.getElementById("toggleBtn");
+const footer = document.getElementById("footer");
+const body = document.body;
+const faSolidOne = document.getElementById("faSolidOne");
+const faSolidTwo = document.getElementById("faSolidTwo");
 
-    alertify.defaults.glossary.title = 'Konfirmasi Pesanan'; 
-    alertify.defaults.glossary.ok = 'Ya, lanjut pesan';
-    alertify.defaults.glossary.cancel = 'Batal';
+alertify.defaults.glossary.title = "Konfirmasi Pesanan";
+alertify.defaults.glossary.ok = "Ya, lanjut pesan";
+alertify.defaults.glossary.cancel = "Batal";
 
-    tombol.addEventListener('click', function(e) {
-        e.preventDefault(); 
-        
-        alertify.confirm('Yakin mau pesan?',
-            function(){
-                alertify.success('Terhubung dengan admin...');
-                teks.innerHTML = "🟢 Pesanan paket landing page berhasil dibuat! Anda terhubung dengan admin untuk proses selanjutnya."
-                teks.style.color = "darkgreen";
-                tombol.innerHTML = "Dipesan";
-                tombol.style.backgroundColor = "lightgrey";
-                tombol.style.border = "none";
-                tombol.style.color = "grey";
-                window.location.href = 'https://wa.me/6281584266490?text=Halo%20Admin%20FyzzCareer%20,%0A%0ASaya%20ingin%20memesan%20paket%20landing%20page%20(paket%20standard)';
-            },
-            function(){
-                alertify.error('Dibatalkan');
-            }
-        );
-    }); 
+tombol.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    alertify.defaults.glossary.title = 'Konfirmasi Pesanan'; 
-    alertify.defaults.glossary.ok = 'Ya, lanjut pesan';
-    alertify.defaults.glossary.cancel = 'Batal';
+  alertify.confirm(
+    "Yakin mau pesan?",
+    function () {
+      alertify.success("Terhubung dengan admin...");
+      teks.innerHTML =
+        "🟢 Pesanan paket landing page berhasil dibuat! Anda terhubung dengan admin untuk proses selanjutnya.";
+      teks.style.color = "darkgreen";
+      tombol.innerHTML = "Dipesan";
+      tombol.style.backgroundColor = "lightgrey";
+      tombol.style.border = "none";
+      tombol.style.color = "grey";
+      faSolidOne.classList.add("fontAwesome-icon-one");
+      window.location.href =
+        "https://wa.me/6281584266490?text=Halo%20Admin%20FyzzCareer%20,%0A%0ASaya%20ingin%20memesan%20paket%20landing%20page%20(paket%20standard)";
+    },
+    function () {
+      alertify.error("Dibatalkan");
+    },
+  );
+});
 
-    button.addEventListener('click', function(e) {
-        e.preventDefault(); 
-        
-        alertify.confirm('Yakin mau pesan?',
-            function(){
-                alertify.success('Terhubung dengan admin...');
-                text.innerHTML = "🟢 Pesanan paket landing page berhasil dibuat! Anda terhubung dengan admin untuk proses selanjutnya."
-                text.style.color = "darkgreen";
-                window.location.href = 'https://wa.me/6281584266490?text=Halo%20Admin%20FyzzCareer%20,%0A%0ASaya%20ingin%20memesan%20paket%20landing%20page%20(paket%20premium)';
-                button.innerHTML = "Dipesan";
-                button.style.backgroundColor = "lightgrey";
-                button.style.border = "none";
-                button.style.color = "grey";
-            },
-            function(){
-                alertify.error('Dibatalkan');
-            }
-        );
-    }); 
+alertify.defaults.glossary.title = "Konfirmasi Pesanan";
+alertify.defaults.glossary.ok = "Ya, lanjut pesan";
+alertify.defaults.glossary.cancel = "Batal";
 
-    // Dark mode 
-    if(localStorage.getItem("theme") === "dark") {
-        body.classList.add("dark-mode");
-        toggleBtn.innerText = "☀️";
-        header.classList.add("header");
-        header.classList.remove("bg-primary");
-        footer.classList.add("footer");
-        footer.classList.remove("bg-primary");
-    } else {
-        toggleBtn.innerText = "🌙";
-        header.classList.remove("header");
-        header.classList.add("bg-primary");
-        footer.classList.remove("footer");
-        footer.classList.add("bg-primary");
-    }
+button.addEventListener("click", function (e) {
+  e.preventDefault();
 
-    toggleBtn.addEventListener("click", () => {
-        body.classList.toggle("dark-mode");
-        if(body.classList.contains("dark-mode")) {
-            localStorage.setItem("theme", "dark");
-            toggleBtn.innerText = "☀️";
-            header.classList.add("header");
-            header.classList.remove("bg-primary");
-            footer.classList.add("footer");
-            footer.classList.remove("bg-primary");
-        } else {
-            localStorage.setItem("theme", "light");
-            toggleBtn.innerText = "🌙";
-            header.classList.remove("header");
-            header.classList.add("bg-primary");
-            footer.classList.remove("footer");
-            footer.classList.add("bg-primary");
-        }
-    });
+  alertify.confirm(
+    "Yakin mau pesan?",
+    function () {
+      alertify.success("Terhubung dengan admin...");
+      text.innerHTML =
+        "🟢 Pesanan paket landing page berhasil dibuat! Anda terhubung dengan admin untuk proses selanjutnya.";
+      text.style.color = "darkgreen";
+      window.location.href =
+        "https://wa.me/6281584266490?text=Halo%20Admin%20FyzzCareer%20,%0A%0ASaya%20ingin%20memesan%20paket%20landing%20page%20(paket%20premium)";
+      button.innerHTML = "Dipesan";
+      button.style.backgroundColor = "lightgrey";
+      button.style.border = "none";
+      button.style.color = "grey";
+      faSolidTwo.classList.add("fontAwesome-icon-two");
+    },
+    function () {
+      alertify.error("Dibatalkan");
+    },
+  );
+});
+
+// Dark mode
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+  toggleBtn.innerText = "☀️";
+  header.classList.add("header");
+  header.classList.remove("bg-primary");
+  footer.classList.add("footer");
+  footer.classList.remove("bg-primary");
+} else {
+  toggleBtn.innerText = "🌙";
+  header.classList.remove("header");
+  header.classList.add("bg-primary");
+  footer.classList.remove("footer");
+  footer.classList.add("bg-primary");
+}
+
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+    toggleBtn.innerText = "☀️";
+    header.classList.add("header");
+    header.classList.remove("bg-primary");
+    footer.classList.add("footer");
+    footer.classList.remove("bg-primary");
+  } else {
+    localStorage.setItem("theme", "light");
+    toggleBtn.innerText = "🌙";
+    header.classList.remove("header");
+    header.classList.add("bg-primary");
+    footer.classList.remove("footer");
+    footer.classList.add("bg-primary");
+  }
+});
