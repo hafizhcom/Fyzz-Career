20249540c27325c3a0923998a4af5a5d685b0439
Fyzz20 <nurhayatiti1122@gmail.com>
2026-05-22 20:10:38 +0700

    update file
    menambahkan efek loadng spinner di paket pembelian

--- toko.js
+++ toko.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 })
 
-// form whatsaap //
+
 document.getElementById("contactForm").addEventListener("submit", function(e) {
     e.preventDefault();
 
@@ -29,7 +29,6 @@ function promo() {
 
     let isMobile = window.innerWidth < 768;
 
-    //fungsi javascript untuk membuat pop up modern dan cantik
     Swal.fire({
     title: '🎓 Promo Pelajar!',
     text: 'CV + Surat Lamaran hanya Rp20.000',
@@ -38,9 +37,10 @@ function promo() {
     width: isMobile ? '90%' : '500px',
     padding: isMobile ? '10px' : '20px',
     });
-
 }
 
+
+// Dark mode toggle
 const toggleBtn = document.getElementById("toggleBtn");
 const body = document.body;
 
@@ -62,3 +62,72 @@ toggleBtn.addEventListener("click", () => {
         toggleBtn.innerText = "🌙";
     }
 });
+
+//library alertify
+const btnProduct = document.getElementById("btnProduct");
+const btnProductTwo = document.getElementById("btnProductTwo");
+const btnProductThree = document.getElementById("btnProductThree");
+const btnProductFour = document.getElementById("btnProductFour");
+
+btnProduct.addEventListener('click', () => {
+    btnProduct.innerHTML = '<i class="fa-solid fa-arrows-rotate" id="spinner1"></i> memuat paket pesanan kamu';
+
+    btnProduct.style.background = "none";
+    btnProduct.style.border = "none";
+    btnProduct.style.color = "grey";
+
+    const spinner1 = document.getElementById("spinner1");
+    spinner1.classList.add('spinner1');
+
+    setTimeout(() => {
+        window.location.href = "produk-cv.html"
+    }, 2000);
+})
+
+btnProductTwo.addEventListener('click', () => {
+    btnProductTwo.innerHTML = '<i class="fa-solid fa-arrows-rotate" id="spinner2"></i> memuat paket pesanan kamu';
+    btnProductTwo.style.background = "none";
+    btnProductTwo.style.border = "none";
+    btnProductTwo.style.color = "grey";
+
+    const spinner2 = document.getElementById("spinner2");
+    spinner2.classList.add('spinner1');
+
+    setTimeout(() => {
+        window.location.href = "produk-portofolio.html"
+    }, 2000);
+})
+
+btnProductThree.addEventListener('click', () => {
+    btnProductThree.innerHTML = '<i class="fa-solid fa-arrows-rotate" id="spinner3"></i> memuat paket pesanan kamu';
+    btnProductThree.style.background = "none";
+    btnProductThree.style.border = "none";
+    btnProductThree.style.color = "grey";
+
+    const spinner3 = document.getElementById("spinner3");
+    spinner3.classList.add('spinner1');
+
+    setTimeout(() => {
+        window.location.href = "produk-makalah.html"
+    }, 2000);
+})
+
+btnProductFour.addEventListener('click', () => {
+    btnProductFour.innerHTML = '<i class="fa-solid fa-arrows-rotate" id="spinner4"></i> memuat paket pesanan kamu';
+    btnProductFour.style.background = "none";
+    btnProductFour.style.border = "none";
+    btnProductFour.style.color = "grey";
+
+    const spinner4 = document.getElementById("spinner4");
+    spinner4.classList.add('spinner1');
+
+    setTimeout(() => {
+        window.location.href = "landing-page.html"
+    }, 2000);
+})
+
+const tulisan = document.getElementById("tulisan");
+
+setInterval(() => {
+    tulisan.textContent = "Hallo, Selamat Datang di FyzzCareer! 👋" ;
+}, 1000);
